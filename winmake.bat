17a43ef07fc6cc4aa68af898dc0c4bf557f703eb
Peter Ferrie <peter.ferrie@gmail.com>
2020-03-11 15:23:51 -0700

    simplify build (#98)

--- winmake.bat
+++ winmake.bat
@@ -1,4 +1,3 @@
-0</* :
 @echo off
 if "%1" equ "clean" (
 echo y|1>nul rd build /s
@@ -9,21 +8,17 @@ setlocal enabledelayedexpansion
 set BUILDDISK=build\passport
 
 set ACME=acme
-set EXOMIZER=exomizer raw -q -P23
+set EXOMIZER=exomizer
 set CADIUS=cadius
 2>nul md build
 cd src\mods
 %ACME% universalrwts.a
 cd ..\..\build
-%EXOMIZER% universalrwts.bin -o universalrwts.tmp
-cscript /nologo //e:jscript %~f0 "b8" "00"
-1>nul copy /b tmp+universalrwts.tmp universalrwts.pak
+%EXOMIZER% mem -lnone -q -P23 -f universalrwts.bin@0xb800 -o universalrwts.pak
 cd ..\src\mods
 %ACME% -r ..\..\build\t00only.lst t00only.a
 cd ..\..\build
-%EXOMIZER% t00only.bin -o t00only.tmp
-cscript /nologo //e:jscript %~f0 "20" "00"
-1>nul copy /b tmp+t00only.tmp t00only.pak
+%EXOMIZER% mem -lnone -q -P23 -f t00only.bin@0x2000 -o t00only.pak
 cd ..\src
 2>..\build\out.txt %ACME% -DFORWARD_DECRUNCHING=1 passport.a
 for /f "tokens=2,3 delims=)" %%q in ('find "RELBASE =" ..\build\out.txt') do set _make=%%q
@@ -52,7 +47,7 @@ for /f "tokens=4,* delims=:(" %%q in ('find "OpenFile=" ..\build\out.txt') do ec
 for /f "tokens=4,* delims=:(" %%q in ('find "ReadFile=" ..\build\out.txt') do echo %%q >> ..\build\vars.a
 for /f "tokens=4,* delims=:(" %%q in ('find "CloseFile=" ..\build\out.txt') do echo %%q >> ..\build\vars.a
 cd ..\build
-%EXOMIZER% -b passport.tmp -o passport.pak
+%EXOMIZER% raw -q -P23 -T4 -b passport.tmp -o passport.pak
 cd ..\src
 %ACME% -DFORWARD_DECRUNCHING=0 wrapper.a
 cd ..
@@ -61,15 +56,9 @@ cd ..
 %CADIUS% ADDFILE "%BUILDDISK%.po" "/PASSPORT/" "build\PASSPORT.SYSTEM"
 cscript /nologo bin/po2do.js build\ build\
 2>nul del "%BUILDDISK%.po"
-goto :EOF
-*/
-new ActiveXObject("scripting.filesystemobject").createtextfile("tmp").write(String.fromCharCode(parseInt(WScript.arguments(0),16),String.fromCharCode(parseInt(WScript.arguments(1),16))))
-/*
-bat/jscript hybrid make script for Windows environments
-a qkumba monstrosity from 2017-10-16
+
 rem third-party tools required to build (must be in path)
 rem https://sourceforge.net/projects/acme-crossass/
 rem https://bitbucket.org/magli143/exomizer/wiki/Home
 rem https://www.brutaldeluxe.fr/products/crossdevtools/cadius/
 rem https://github.com/mach-kernel/cadius
-*/
